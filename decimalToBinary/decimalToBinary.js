4a3e581968269124bd39f012c307abf713745062
Chigozie Orunta <chigozieorunta@yahoo.com>
2020-01-04 12:20:46 +0100

    Update decimalToBinary.js

--- decimalToBinary/decimalToBinary.js
+++ decimalToBinary/decimalToBinary.js
@@ -2,7 +2,7 @@ function decimalToBinary(N) {
     let binaryNumber = "";
     while(N > 0) {
         binaryNumber = binaryNumber + (N%2) + "";
-        N = parseInt(N/2);
+        N = Math.floor(N/2);
     }
     return parseInt(binaryNumber);
 }
