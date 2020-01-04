ace0baebaba04be1a87a4a01e7871ecc88888e8e
Chigozie Orunta <chigozieorunta@yahoo.com>
2020-01-04 14:12:12 +0100

    Update binaryGap.js

--- binaryGap/binaryGap.js
+++ binaryGap/binaryGap.js
@@ -1,15 +1,11 @@
 function solution(N) {
     N = N.toString(2);
     let numberOfOnes = 0;
-    let numberOfZeros = 0;
     let binaryGap = 0;
-    let markPosition = false;
     let binaryZeros = 0;
     for(let i=0; i<N.length; i++) {
         if(N[i] > 0) {
             numberOfOnes++;
-        } else {
-            numberOfZeros++;
         }
     }
     if(numberOfOnes) {
