87014268fd0e6a33503d3389b33ec41a4993e85e
Chigozie Orunta <chigozieorunta@yahoo.com>
2020-01-04 13:51:53 +0100

    Update factorial.js

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,8 +1,9 @@
 function solution(N) {
-  let factorial = 1;
-  while(N > 0) {
-    factorial *= (N-1);
-    N--;
-  }
-  return factorial;
+    let factorial = N;
+    if(N) { factorial = N; } else { factorial = 1; }
+    while(N > 1) {
+        factorial *= (N-1);
+        N--;
+    }
+    return factorial;
 }
