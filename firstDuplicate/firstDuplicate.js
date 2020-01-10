b9a2ef3d9c2e9df87cc1b9b9cb2ed98ee7999785
chigozieorunta <chigozieorunta@yahoo.com>
2020-01-10 11:21:27 +0100

    Added firstDuplicate.js file

--- firstDuplicate/firstDuplicate.js
+++ firstDuplicate/firstDuplicate.js
@@ -1,1 +1,13 @@
+function solution(N) {
+    let O = [];
+    for(let i=0; i<N.length; i++) {
+        if(O.indexOf(N[i]) < 0) {
+            O.push(N[i]);
+        } else {
+            return N[i];
+        }
+    }
+}
 
+a = [2, 1, 3, 5, 3, 2];
+console.log(solution(a));
