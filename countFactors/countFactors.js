7e22f575f1940a70396b8f1d1e85af9d5618a435
chigozieorunta <chigozieorunta@yahoo.com>
2020-01-12 00:25:30 +0100

    Added countFactors code snippet

--- countFactors/countFactors.js
+++ countFactors/countFactors.js
@@ -1,10 +1,25 @@
 function solution(N) {
     let numberOfFactors = 0;
-    if(N%2) {
-        if(N%3) {
-            return 2;
+    let bool = true;
+    if(N === 1) return 1;
+    function isPrime(num) {
+        if (num <= 1) {
+          return true
+        } else if (num <= 3) {
+          return true
+        } else if (num%2 === 0 || num%3 === 0) {
+          return false
         }
+        let i = 5
+        while (i*i <= num) {
+          if (num%i === 0 || num%(i+2) === 0) {
+            return false
+          }
+          i += 6
+        }
+        return true
     }
+    if(isPrime(N)) return 2;
     let A = N;
     for(let i=1; i<A; i++) {
         if(!(N%i)) {
